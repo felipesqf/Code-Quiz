c1b499db06b93f3313de74759ce3486322b67f14
^Felipe <felipesqf@gmail.com>
2020-09-09 22:15:29 +1000

    beta version

---
+++ script.js
@@ -0,0 +1,37 @@
+​var startQuizButton = document.getElementById('startquiz');
+startQuizButton.addEventListener('click', startQuiz);
+
+   
+
+var questionsArray = [
+    {
+        questionText: "Commonly used data types DO NOT inlude:",
+        answers: ["alerts", "booleans", "numbers", "strings"],
+        answerId: 0
+    },
+    {
+        questionText: "The condition in an if/else statement is enclosed within __________",
+        answers: ["curly braces", "parenthesis", "quotes", "square brackets"],
+        answerId: 1
+    },
+    {
+        questionText: "Arrays in JavaScript can be used to store __________",
+        answers: ["booleans", "numbers and strings", "other arrays", "all of the above"],
+        answerId: 3
+    },
+    {
+        questionText: "String values must be enclosed within __________ when being assigned to variables.",
+        answers: ["commas", "curly braces", "quotes", "parenthesis"],
+        answerId: 2
+    },
+    {
+        questionText: "A very useful tool used during development and debugging for printing content to the degugger is:",
+        answers: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answerId: 3
+    }
+]
+function startQuiz(){
+    alert(questionsArray);
+}
+
+
